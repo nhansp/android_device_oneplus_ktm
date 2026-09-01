2b59e5b67d4a9e3cbe3fb0b6cf9674c1b819d046
nhansp <nhan.094.vn@gmail.com>
2026-09-01 08:48:45 +0000

    ktm: Ship the stock camera assets

--- device.mk
+++ device.mk
@@ -76,3 +76,7 @@ $(call inherit-product, device/oneplus/sm8750-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/ktm/ktm-vendor.mk)
+
+# Inherit the stock camera assets. Ships the odm/etc/camera files our blob list
+# misses; the OPlus camera app itself stays behind TARGET_SHIP_OPLUS_CAMERA_APP.
+$(call inherit-product, vendor/oneplus/ktm-opluscamera/ktm-opluscamera-vendor.mk)
