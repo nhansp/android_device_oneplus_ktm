8d78d14afa7c36f133cc283c370a1a920824ab83
nhansp <nhan.094.vn@gmail.com>
2026-09-01 08:43:39 +0000

    ktm: Stop disabling camera face detection

--- extract-files.py
+++ extract-files.py
@@ -24,9 +24,6 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
-    'odm/etc/camera/CameraHWConfiguration.config': blob_fixup()
-        .regex_replace(r'(enableSWfdForThirdCamUnit += )TRUE', r'\1FALSE')
-        .regex_replace(r'(fdSupport += )TRUE;', r'\1FALSE;'),
     'odm/etc/init/init.camera_process.rc': blob_fixup()
         .regex_replace('    delete_recursion', '    #delete_recursion'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
